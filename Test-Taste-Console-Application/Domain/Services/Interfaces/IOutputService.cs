8b709d75bec937c789d6950e8665cb9adf121499
Linesh Lanjewar <lineshlanjewar@gmail.com>
2025-04-05 23:38:04 +0530

    added table for panets and the average of their moon's temperature

--- Test-Taste-Console-Application/Domain/Services/Interfaces/IOutputService.cs
+++ Test-Taste-Console-Application/Domain/Services/Interfaces/IOutputService.cs
@@ -1,12 +1,15 @@
-﻿namespace Test_Taste_Console_Application.Domain.Services.Interfaces
+﻿using Test_Taste_Console_Application.Domain.Objects;
+
+namespace Test_Taste_Console_Application.Domain.Services.Interfaces
 {
     ///<summary>
     /// An output service that can show data from the Solar System OpenData API<see href="https://api.le-systeme-solaire.net/"/> to a user via the console. 
     ///</summary>
     public interface IOutputService
     {
-        void OutputAllPlanetsAndTheirMoonsToConsole();
+        void OutputAllPlanetsAndTheirMoonsToConsole(Planet[] planets);
         void OutputAllMoonsAndTheirMassToConsole();
-        void OutputAllPlanetsAndTheirAverageMoonGravityToConsole();
+        void OutputAllPlanetsAndTheirAverageMoonGravityToConsole(Planet[] planets);
+        void OutputAllPlanetsAndTheirMoonsAvgTemperature(Planet[] planets);
     }
 }
